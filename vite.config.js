b6fbbab18c7518fc94d9b2de675e70d306277d3b
Cristian Gomez <cristian.gomez.r1089@gmail.com>
2026-06-26 15:53:55 -0300

    npm run build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/talento-it-landing/",
   plugins: [
     react(),
     tailwindcss(),
