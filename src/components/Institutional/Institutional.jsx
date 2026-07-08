f55c1c0df2e063cdfb585b795e924cb047ef8ef8
Natalia <colibri.postman@gmail.com>
2026-07-07 23:59:13 -0300

    modificado src/components/Institutional/Institutional.jsx

--- src/components/Institutional/Institutional.jsx
+++ src/components/Institutional/Institutional.jsx
@@ -1,5 +1,5 @@
 import potreroLogo from "../assets/company-logos/potrero_logo.png";
-import compromisoLogo from "../assets/company-logos/Logo Compromiso - Principal.png";
+import compromisoLogo from "../assets/company-logos/Logocompromiso.png";
 import {
   institutionalPartners,
   institutionalPillars,
